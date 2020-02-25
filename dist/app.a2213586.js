5e88bcf676e7617e4613f04571d6e00d2c8acdba
Ana Martins <anacarolcm@gmail.com>
2020-02-25 21:00:25 +0100

    Share at Twitter button on modal

--- dist/app.a2213586.js
+++ dist/app.a2213586.js
@@ -33438,7 +33438,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56675" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56752" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
